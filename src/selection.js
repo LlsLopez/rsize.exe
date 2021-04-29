adc671ed4e367371539b75fa47e5e053d6cccd6b
Armando <llslopezarmando@csu.fullerton.edu>
2021-04-28 23:07:57 -0700

    commit

---
+++ src/selection.js
@@ -0,0 +1,43 @@
+var s1 = document.getElementById("s1");
+var s2 = document.getElementById("s2");
+var s3 = document.getElementById("s3");
+var s4 = document.getElementById("s4");
+var s5 = document.getElementById("s5");
+var s6 = document.getElementById("s6");
+var s7 = document.getElementById("s7");
+var s8 = document.getElementById("s8");
+var s9 = document.getElementById("s9");
+var s10 = document.getElementById("s10");
+var s11 = document.getElementById("s11");
+var s12 = document.getElementById("s12");
+var s13 = document.getElementById("s13");
+var s14 = document.getElementById("s14");
+var s15 = document.getElementById("s15");
+var s16 = document.getElementById("s16");
+
+exercises = ["empty","1","2","3","4","5","6","7","8","9","10",
+"11","12","13","14","15","16","17"];
+
+
+selectNum = 16;
+pageNum = 0;
+
+s1.innerHTML = exercises[pageNum * 16 + 1];
+s2.innerHTML = exercises[pageNum * 16 + 2];
+s3.innerHTML = exercises[pageNum * 16 + 3];
+s4.innerHTML = exercises[pageNum * 16 + 4];
+s5.innerHTML = exercises[pageNum * 16 + 5];
+s6.innerHTML = exercises[pageNum * 16 + 6];
+s7.innerHTML = exercises[pageNum * 16 + 7];
+s8.innerHTML = exercises[pageNum * 16 + 8];
+s9.innerHTML = exercises[pageNum * 16 + 9];
+s10.innerHTML = exercises[pageNum * 16 + 10];
+s11.innerHTML = exercises[pageNum * 16 + 11];
+s12.innerHTML = exercises[pageNum * 16 + 12];
+s13.innerHTML = exercises[pageNum * 16 + 13];
+s14.innerHTML = exercises[pageNum * 16 + 14];
+s15.innerHTML = exercises[pageNum * 16 + 15];
+s16.innerHTML = exercises[pageNum * 16 + 16];
+
+
+
---
+++ src/stopWatch.js
@@ -0,0 +1,96 @@
+console.log("test");
+var start = document.getElementById("start");
+var stop = document.getElementById("stop");
+var reset = document.getElementById("reset");
+var miliseconds = document.getElementById("miliseconds");
+var seconds = document.getElementById("seconds");
+var minutes = document.getElementById("minutes");
+var hours = document.getElementById("hours");
+
+var msCounter = 0;
+var secCounter = 0;
+var minCounter = 0;
+var hourCounter = 0;
+var preventStartSpam = false;
+
+function startStopWatch(){
+    msCounter++;
+    if(msCounter < 9)
+    {
+        miliseconds.innerHTML = "0" + msCounter;
+    }
+    if(msCounter > 9 && msCounter < 100) {
+        miliseconds.innerHTML = msCounter;
+    }
+    if(msCounter > 99)
+    {
+        secCounter += 1;
+        seconds.innerHTML = "0" + secCounter;
+        msCounter = 0;
+        miliseconds.innerHTML = "00";
+    }
+    if(secCounter > 9)
+    {
+        seconds.innerHTML = secCounter;
+    }
+    if(secCounter > 59)
+    {
+        minCounter += 1;
+        minutes.innerHTML = "0" + minCounter;
+        secCounter = 0;
+        seconds.innerHTML = "00";
+        msCounter = 0;
+        miliseconds.innerHTML = "00";
+    }
+    if(minCounter < 9)
+    {
+        minutes.innerHTML = "0" + minCounter;
+    }
+    if(minCounter > 9)
+    {
+        minutes.innerHTML = minCounter;
+    }
+    if(minCounter > 59)
+    {
+        hourCounter += 1;
+        hours.innerHTML = "0" + hourCounter;
+        minCounter = 0;
+        minutes.innerHTML = "00";
+        secCounter = 0;
+        seconds.innerHTML = "00";
+        msCounter = 0;
+        miliseconds.innerHTML = "00";
+    }
+    if(hourCounter < 9)
+    {
+        hours.innerHTML = "0" + hourCounter;
+    }
+    if(hourCounter > 9)
+    {
+        hours.innerHTML = hourCounter;
+    }
+}
+
+start.onclick = function ()
+{
+    if(preventStartSpam == false)
+    {
+    interval = setInterval(startStopWatch,10);
+    preventStartSpam = true;
+    }
+}
+stop.onclick = function () {
+    clearInterval(interval);
+    preventStartSpam = false;
+}
+reset.onclick = function () {
+    clearInterval(interval);
+    msCounter = 0;
+    miliseconds.innerHTML = "00";
+    secCounter = 0;
+    seconds.innerHTML = "00";
+    minCounter = 0;
+    minutes.innerHTML = "00";
+    hourCounter = 0;
+    hours.innerHTML = "00";
+}
